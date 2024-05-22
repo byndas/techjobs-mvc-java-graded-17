354d40de0d4029d85b14ec85a8181bceeb3b3e08
Marc Byndas <byndasma@gmail.com>
2024-05-22 19:47:37 -0400

    graded assignment #3 condensed & clarified bonus instructions

--- src/main/java/org/launchcode/techjobsmvc/TechjobsMvcApplication.java
+++ src/main/java/org/launchcode/techjobsmvc/TechjobsMvcApplication.java
@@ -11,6 +11,8 @@ public class TechjobsMvcApplication {
 }
 
 /*
+ASSIGNMENT OUTLINE:
+
 “model” is in models.JobData class
 this class isn’t a typical MVC/OOP model
 
@@ -59,25 +61,35 @@ SearchController:
 	search controller contains only search()
 	 displays form defined in the search.html template.
 
-Later in this assignment, you will receive instructions for adding a second handler to deal with user input and display the search results.
+await instructions for second user input handler displaying search results
 
-The Views
-Let’s turn our attention to the views.
+Views:
 
-The fragments File
-If the application is not running, launch it and navigate to the site’s home page in your browser. Also open up the src/main/resources/templates/index.html file in IntelliJ. You’ll notice that there is a fair amount of content visible on the page that isn’t contained in index.html. This is because we’re using two fragments from fragments.html (head and page-header). These allow for some basic page structure and navigation to be shared across all of our views.
+	in fragments File,
+		if app not running,
+			launch & navigate to homepage
+				open src/main/resources/templates/index.html
+					some content isn’t contained in index.html
+						due to using two fragments from fragments.html (head & page-header)
+							allowing basic page structure & shared navigation across all views
 
-Have a look at the structure of fragments.html, but you will NOT need to do any work within this file for this assignment.
+	look at structure of fragments.html -- don't touch this file
 
- Tip
-We use Twitter’s Bootstrap CSS, HTML, and JS framework to provide some styling and functionality to our views. The appropriate files are included at the top of fragments.html and thus are included on every page of our app.
+	Tip -- using Twitter Bootstrap (CSS, HTML, JS) framework
+		appropriate files included atop fragments.html
+	    thus included on each app page
 
-You won’t have to explicitly use Bootstrap at all in this assignment, but it’s a great way to make your sites look good with minimal work. Consider using it in your own projects!
+	No need to explicitly use Bootstrap in this assignment
+		but makes sites look good with minimal work
 
-The List Views
-Turn your attention to list.html. This page displays a table of links broken down into several categories. Data from columnChoices is used to fill in the header row, and information stored in tableChoices generates the link text.
+List Views:
 
-The most interesting part of this template is how we generate the links:
+	list.html displays table of link categories
+
+	columnChoices data used in header row,
+		tableChoices data generates link text
+
+	to generate links:
 
    <td th:each="category : ${tableChoices}">
       <ul>
@@ -86,28 +98,112 @@ The most interesting part of this template is how we generate the links:
          </li>
       </ul>
    </td>
-tableChoices is a HashMap from JobData, and it contains the names of the Job fields as keys (employer, etc.). The value for each key is an ArrayList of Employer, Location, CoreCompetency, or PositionType objects.
-In line 1, category represents one key/value pair from tableChoices, and in line 3, item represents one entry from the stored ArrayList.
-We’ve seen the syntax @{/list/jobs} to generate a link within a Thymeleaf template, but we haven’t seen the other portion of the link: (column=${category.key},value=${position}). This syntax causes Thymeleaf to dynamically generate query parameters for our URL.
-In line 4, we set these parameters by using column= and value=. The values of these parameters are determined dynamically based on ${category.key} and ${item}. Since these values come from tableChoices, the keys will be employer, location, etc. The values will be the individual elements from the related ArrayList. When the user clicks on these links, they will be routed to the listJobsByColumnAndValue handler in ListController, which looks for these parameters.
 
-Clicking on one of the links will display a list of jobs that relate to the choice, via the listJobsByColumnAndValue handler method. However, that view, list-jobs.html isn’t working yet. While the handler method is fully implemented, the view template needs some work.
+tableChoices is HashMap from JobData containing names of Job fields as keys
+	 each key value is ArrayList of Employer, Location, CoreCompetency, or PositionType objects
 
-For now, click one of the Location links. This sends a request as we outlined above, but doing so only displays a page with a title and no job list.
+line 1, category represents a key/value pair from tableChoices,
+line 3, item represents an entry from stored ArrayList
+	We saw syntax @{/list/jobs} generating link in Thymeleaf template,
+		we haven’t seen the other link portion:
+	    (column=${category.key},value=${position})
+	      causing Thymeleaf to dynamically generate query parameters for our URL
+line 4, sets these parameters values via column= & value=
+	depending on ${category.key} & ${item}
+	  since these values come from tableChoices,
+	    keys will be employer, location, etc.
+	      values will be individual elements from related ArrayList
+When user clicks link,
+	routed to listJobsByColumnAndValue handler in ListController,
+		which looks for these parameters
 
-The page you see at /list/values?column=location&value=... is generated by the list-jobs.html template. It has a similar structure as list.html, but the table consists of only one column.
+Clicking link displays list of jobs relating to choice,
+	via listJobsByColumnAndValue handler method
+	  yet list-jobs.html view fails to work
+	    handler method fully implemented,
+	      but view template needs some work
 
- Note
-Select “Kansas City” from the list of locations, and then check the address bar of your browser:
+click a Location link sending request as outlined above,
+  doing so only displays page with title & no job list
 
-   /list/jobs?column=location&value=Kansas%20City
-Thymeleaf inserts %20 for us, to represent a space, but this may actually be hidden in your browser’s address bar.
+list-jobs.html displays page /list/values?column=location&value=...
+	similar structure to list.html but table has only one column
 
-The Search View
-Finally, click on Search from the home page, or the navigation bar, and open up search.html in IntelliJ. You’ll see a search form (in both the browser and template file) that gives the user the option of searching by a given Job field, or across all fields. This is an exact visual analog of our console application.
+select “Kansas City” from the list of locations,
+	check address bar:
+		/list/jobs?column=location&value=Kansas%20City
+			Thymeleaf inserts %20 for us, to represent a space
+				but may be hidden in your browser’s address bar
 
-This template will be used to display search results, in addition to rendering the form. This will give the nice user experience of easily searching multiple times in a row.
+Search View:
+	finally, click Search from home page or nav bar,
+		open search.html
+		  search form (in both browser & html file) gives user option to search
+		    by given Job field or across all fields
+		      an exact visual analog of our console app
 
-Wrap Up the Code Review
-Once you understand the controllers and views that are already in place, you’re ready to begin your work.
+	this template displays search results & renders form
+    this gives user nice experience to easily search multiple times
+
+Finish Code Review:
+	understand repo controllers & views then begin working
+
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
+Bonus
+
+when selecting given field to search & submit,
+  our choice is forgotten & returns to “All” by default
+    modify html to keep previous search field selected when displaying results
+
+in tables displaying full job data, manipulate font, style, capitalization, etc.
+  to further distinguish labels from data (e.g. Employer: LaunchCode)
+
+Hint: title string capitalized in multiple templates
+
+in tables of job results,
+	make each value (except name) hyperlinked
+	  to new listing of all jobs with same value
+      for example, if a list of jobs with JS skill,
+        clicking on location generates new list
+          with all jobs available there
+
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
+Super Bonus
+
+actionChoices HashMap passed to view in HomeController.index method
+  HomeController is responsible for presenting actions, not the view,
+    but we did not do this for nav links displayed on every page
+
+In order to similarly detach navigation links,
+	must pass actionChoices to every view,
+    since nav links generated in fragments.html
+	must do like line below in every handler method
+    which is a pain & error-prone & difficult to update
+	    model.addAttribute("actions", actionChoices);
+
+to fix this:
+
+	make new controller, TechJobsController
+	  having static HashMap, actionChoices
+	    populated via no-argument constructor,
+	      as columnChoices populated in ListController
+
+	do not add @Controller annotation to this class
+
+	write static method getActionChoices in TechJobsController
+	  returning actions HashMap
+
+	add annotation @ModelAttribute("actions") to getActionChoices method
+	 causing method's return value to be set in model with key "actions"
+	  for each controller extending TechJobsController
+
+	modify all other controllers to extend TechJobsController
+
+	modify fragments.html to use passed-in action choices to generate nav links
+
+	place columnChoices in TechJobsController
 */
